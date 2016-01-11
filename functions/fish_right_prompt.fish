1dd23a737782ee407ec939e22f20d76c53baf301
Jeff Sandberg <paradox460@gmail.com>
2016-01-11 10:39:10 -0800

    Remove cursor control characters from right prompt
    They were fucking up the prompt when it was only 1 line long, as opposed to being 2 lines.

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -2,12 +2,10 @@ function fish_right_prompt
   if status --is-interactive
     set -l __paradox_date (command date +%I:%M:%S%P ^/dev/null)
     if type ruby >/dev/null ^/dev/null
-      # have to use the echo inline to prevent obnoxious spaces, i.e. (💎2.3.0 )
+      # have to use the echo inline to prevent obnoxious spaces, i.e. (💎 2.3.0 )
       set __paradox_rb_version (echo -s "(💎  " (command ruby -e 'print RUBY_VERSION' ^/dev/null) ")")
     end
 
-    # The \e[1A code below is to move the prompt up a line, so it aligns with the first line of the lprompt.
-    # The \e[1B code resets to the previous line, so everything renders properly
-    echo -se "\e[1A" (set_color blue) "$__paradox_rb_version" (set_color green) (__paradox_command_duration) (set_color normal) "[$__paradox_date]"  "\e[1B"
+    echo -se (set_color blue) "$__paradox_rb_version" (set_color green) (__paradox_command_duration) (set_color normal) "[$__paradox_date]"
   end
 end
