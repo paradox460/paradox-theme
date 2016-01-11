b6e14efae31c4cec967afe51d5773f3d8d709a90
Jeff Sandberg <paradox460@gmail.com>
2016-01-10 20:21:43 -0800

    Allow git_hash to be run uninteractively

--- functions/__paradox_git_hash.fish
+++ functions/__paradox_git_hash.fish
@@ -1,18 +1,16 @@
 function __paradox_git_hash --description "Get the current git commit hash, if we're in a git working tree"
-  if status --is-interactive
-    set -l git_command 'git rev-parse --is-inside-work-tree --short HEAD ^/dev/null'
+  set -l git_command 'git rev-parse --is-inside-work-tree --short HEAD ^/dev/null'
 
-    if type gtimeout >/dev/null ^/dev/null
-      set  -l git_command 'timeout 1' git_command
-    end
+  if type gtimeout >/dev/null ^/dev/null
+    set  -l git_command 'timeout 1' git_command
+  end
 
-    set -l git_info (eval $git_command)
-    if test (count $git_info) -gt 1
-      set -l inside_worktree $git_info[1]
-      set -l git_head $git_info[2]
-      if test "true" = "$inside_worktree"
-        echo -s (set_color blue) "[$git_head]"
-      end
+  set -l git_info (eval $git_command)
+  if test (count $git_info) -gt 1
+    set -l inside_worktree $git_info[1]
+    set -l git_head $git_info[2]
+    if test "true" = "$inside_worktree"
+      echo -s (set_color blue) "[$git_head]"
     end
   end
 end
