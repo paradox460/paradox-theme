0f2ba726d899855858f930e154cf42c555f18bfc
Jeff Sandberg <paradox460@gmail.com>
2016-07-26 11:11:15 -0700

    Update timestamp to actually show meridian state

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,6 +1,6 @@
 function fish_right_prompt
   if status --is-interactive
-    set -l __paradox_date (command date +%I:%M:%S%P ^/dev/null)
+    set -l __paradox_date (command date +%I:%M:%S%p ^/dev/null)
     if type -q ruby
       # have to use the echo inline to prevent obnoxious spaces, i.e. (💎 2.3.0 )
       set __paradox_rb_version (echo -s "(💎  " (command ruby -e 'print RUBY_VERSION' ^/dev/null) ")")
